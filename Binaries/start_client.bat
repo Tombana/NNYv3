5c473d17cf37db3200558393ef4e4887501d3a4c
Alex Belanger <i.cought.air@gmail.com>
2010-05-16 17:33:07 -0400

    Binaries file removed because of GitHub's policy (limited storage); Config files cleaned back  to the default example; Merged Tombana's commits (CaptureConsoleOutput branch excluded)

--- Binaries/start_client.bat
+++
@@ -1,4 +0,0 @@
-@echo OFF
-cd ../Dependencies && copy pthreadVCE2.dll ..\Binaries
-cd ../Dependencies/Ogre3D && "../../Binaries/NNYv3 Client.exe"
-del ..\..\Binaries\pthreadVCE2.dll
--- Binaries/start_world.bat
+++
@@ -1,2 +0,0 @@
-@echo OFF
-cd ../Dependencies && "../Binaries/NNYv3 World Server.exe"
