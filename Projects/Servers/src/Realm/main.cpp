abb1b4b87cfae69a127033ecbd7bb4fd76a986c8
Alex Belanger <i.cought.air@gmail.com>
2010-06-01 19:17:34 -0400

    Final working tree; World server and Realm server projects are now parts of the same VC solution as they will both uses ACE and Common files such as protocol.h and more (I'm thinking of a single database for the Realm & World server too). That's such a lot of work but it's better this way for sure. Sorry for delaying you, I'll be working a LOT on the projects now that school's almost over (:

---
+++ Projects/Servers/src/Realm/main.cpp
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int main(int argc, char **argv) {
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
---
+++ Projects/Servers/src/Realm/main.h
@@ -0,0 +1,8 @@
+#ifndef H_MAIN
+#define H_MAIN
+
+#include <iostream>
+
+int main(int argc, char **argv);
+
+#endif
